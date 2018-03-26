bf6195cc532c1755de30940e02c1ae3fd8ae989a
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-03-26 07:28:34 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-recognition.lang = 'en-US';
-recognition.interimResults = false;
-recognition.maxAlternatives = 5;
-recognition.start();
+window.onload = recognizeSpeech;
 
-recognition.onresult = function(event) {
-	var result = event.results[0][0].transcript;
-	document.getElementById("result").innerHTML = result;
-	document.getElementById("confidence").innerHTML = event.results[0][0].confidence;
-	alert((spamWords.filter(x=>result.indexOf(word) !=1).length !== 0));
-};
+function recognizeSpeech() {
+	var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+	recognition.lang = 'en-US';
+	recognition.interimResults = false;
+	recognition.maxAlternatives = 5;
+	recognition.start();
 
-var spamWords = ['fortnight','viagra','can you help me'];
+	recognition.onresult = function(event) {
+		var result = event.results[0][0].transcript;
+		document.getElementById("result").innerHTML = result;
+		document.getElementById("confidence").innerHTML = event.results[0][0].confidence;
+		alert((spamWords.filter(x=>result.indexOf(word) !=1).length === 0));
+	};
+
+	var spamWords = ['fortnight','viagra','can you help me','install','free','lol'];
+}
