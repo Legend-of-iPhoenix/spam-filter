853ab4b824e8c0ea0725c0080949af04e9609a37
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-03-26 07:15:10 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ recognition.maxAlternatives = 5;
 recognition.start();
 
 recognition.onresult = function(event) {
-	document.getElementById("result").innerHTML = event.results[0][0].transcript;
+	var result = event.results[0][0].transcript;
+	document.getElementById("result").innerHTML = result;
 	document.getElementById("confidence").innerHTML = event.results[0][0].confidence;
+	alert((spamWords.filter(x=>result.indexOf(word) !=1).length !== 0));
 };
+
+var spamWords = ['fortnight','viagra','can you help me'];
